0ffdb5c71457b7bec38c4c56067579e16a7ebabe
Pavlíček Vilém <pavlicek@ttc.cz>
2025-12-22 15:11:17 +0100

    add states

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -2,11 +2,13 @@ use bevy::prelude::*;
 use village::VillagePlugin;
 use portals::PortalsPlugin;
 use wallet::WalletPlugin;
+use states::GameState;
 
 pub struct CorePlugin;
 
 impl Plugin for CorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins((VillagePlugin, PortalsPlugin, WalletPlugin));
+        app.init_state::<GameState>()
+            .add_plugins((VillagePlugin, PortalsPlugin, WalletPlugin));
     }
 }
---
+++ states/src/lib.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::*;
+
+#[derive(States, Default, Debug, Clone, PartialEq, Eq, Hash)]
+pub enum GameState {
+    #[default]
+    Loading,
+    Running,
+}
