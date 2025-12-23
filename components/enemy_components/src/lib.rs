cc06ab79e1c53c158a951019d44d9c8af49a34f6
Pavlíček Vilém <pavlicek@ttc.cz>
2025-12-23 16:33:06 +0100

    add enemy_components lib & goblin.scn.ron

---
+++ components/enemy_components/src/lib.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct Enemy;
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct MovementSpeed(pub f32);
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct RewardCoefficient(pub f32);
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct NeedsHydration;
