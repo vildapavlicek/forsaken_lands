c25c6ce4556118f0a3dc3cb2a12d449427723413
Pavlíček Vilém <pavlicek@ttc.cz>
2025-12-30 19:42:32 +0100

    add resource wallet

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,9 +1,16 @@
 use bevy::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Resource, Reflect, Default, Debug, Clone)]
+#[reflect(Resource, Default)]
+pub struct Wallet {
+    pub resources: HashMap<String, u32>,
+}
 
 pub struct WalletPlugin;
 
 impl Plugin for WalletPlugin {
-    fn build(&self, _app: &mut App) {
-        // Empty for now
+    fn build(&self, app: &mut App) {
+        app.register_type::<Wallet>().init_resource::<Wallet>();
     }
 }
