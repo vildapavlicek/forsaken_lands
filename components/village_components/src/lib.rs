39c7089826f1985fd329026b651edce73e5163bd
Pavlíček Vilém <pavlicek@ttc.cz>
2026-01-02 00:35:38 +0100

    define EnemyEncyclopedia

--- components/village_components/src/lib.rs
+++ components/village_components/src/lib.rs
@@ -1,5 +1,18 @@
 use bevy::prelude::*;
+use std::collections::HashMap;
+
+#[derive(Reflect, Default, Debug, Clone)]
+pub struct EncyclopediaEntry {
+    pub kill_count: u64,
+}
+
+#[derive(Component, Reflect, Default, Debug, Clone)]
+#[reflect(Component)]
+pub struct EnemyEncyclopedia {
+    pub inner: HashMap<String, EncyclopediaEntry>,
+}
 
 #[derive(Component, Reflect, Default, Debug, Clone, PartialEq, Eq, Hash)]
 #[reflect(Component)]
+#[require(EnemyEncyclopedia)]
 pub struct Village;
--- village/src/lib.rs
+++ village/src/lib.rs
@@ -1,9 +1,14 @@
-use {bevy::prelude::*, village_components::Village};
+use {
+    bevy::prelude::*,
+    village_components::{EncyclopediaEntry, EnemyEncyclopedia, Village},
+};
 
 pub struct VillagePlugin;
 
 impl Plugin for VillagePlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<Village>();
+        app.register_type::<EnemyEncyclopedia>();
+        app.register_type::<EncyclopediaEntry>();
     }
 }
