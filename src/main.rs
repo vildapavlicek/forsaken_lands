7b3aa50f5365b5799979224005259bf0f208ecae
Pavlíček Vilém <pavlicek@ttc.cz>
2026-01-25 19:57:15 +0100

    logs

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,15 @@ fn main() {
     App::new()
         .add_plugins(
             DefaultPlugins.set(LogPlugin {
-                filter:
-                    "error,loading=trace,portals=debug,village=debug,wallet=debug,heroes=debug,unlocks=info,save_load=trace"
-                        .into(),
+                filter: "error,loading=trace,\
+                    portals=debug,\
+                    village=debug,\
+                    wallet=debug,\
+                    heroes=debug,\
+                    unlocks=info,\
+                    save_load=trace,\
+                    village_ui=debug"
+                    .into(),
                 level: bevy::log::Level::TRACE,
                 ..Default::default()
             }),
